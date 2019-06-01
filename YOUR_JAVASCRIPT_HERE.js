0af7b84a28a1abcb9e26734823bc3e59f1edc746
jamiekim828 <jamiekim828@gmail.com>
2019-06-01 10:28:37 +0200

    rest,reset,pickUpItem function

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,29 @@
 // Write your JS here
+var hero = {
+    name: 'Genie',
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon : {
+        type: 'Lamp',
+        damage: 2
+    }
+}
+function rest(hero) {
+    if (hero.health === 10) {
+        window.alert('You are strong enough!');
+    } else {
+        hero.health = 10;
+    }
+    return hero;
+}
+window.onload = function() {
+    document.getElementById("inn").onclick = function reset() {
+        rest(hero);
+    }
+}
+function pickUpItem(hero,weapon) {
+    var newLength = hero.inventory.push(weapon);
+}
+
+module.exports = {hero,rest,reset,pickUpItem,equipWeapon};
